6b4357e9572953c6166ebc425b4e32658c1875b7
Mario Roy <marioeroy@gmail.com>
2025-11-27 23:45:06 -0500

    Refactor
    - Switch to Ubuntu 22.04 for the container
    - Build x86_64,i386 Portable Executable Wine
    
    The result is smaller container and faster build time.

--- build-wine.sh
+++ build-wine.sh
@@ -13,90 +13,59 @@ if [ "$BUILD_WAYLAND" = "0" ]; then
   echo "The build will skip the Wine Wayland driver."
 fi
 
-flags=("-mfpmath=sse" "-std=gnu17" "-O2" "-ftree-vectorize" "-pipe")
+flags=(
+  "-std=gnu17" "-msse3" "-mfpmath=sse" "-mpopcnt"
+  "-O2" "-ftree-vectorize" "-pipe"
+)
+
+[ "$BUILD_DEBUG" = "1" ] && flags+=("-g")
 
 nowarnings=(
   "-Wno-discarded-qualifiers"
   "-Wno-format"
   "-Wno-maybe-uninitialized"
   "-Wno-misleading-indentation"
+  "-Wno-stringop-overflow"
 )
 
-# Generic flags
-export CFLAGS="-march=x86-64 -msse3 -mpopcnt ${flags[*]} ${nowarnings[*]} -ffat-lto-objects"
+# Generic and cross-compilation flags
+export CFLAGS="-march=x86-64 ${flags[*]} ${nowarnings[*]}"
+export CXXFLAGS="-march=x86-64 ${flags[*]} ${nowarnings[*]}"
+export LDFLAGS="-Wl,-O1,--sort-common,--as-needed"
 
-# Flags for cross-compilation
-export CROSSCFLAGS="-march=i686 -msse2 ${flags[*]} ${nowarnings[*]}"
-export CROSSCXXFLAGS="-march=i686 -msse2 ${flags[*]} ${nowarnings[*]}"
-export CROSSLDFLAGS="-Wl,-O1"
+export CROSSCC="x86_64-w64-mingw32-gcc"
+export CROSSCXX="x86_64-w64-mingw32-g++"
+export CROSSCFLAGS="${CFLAGS}"
+export CROSSCXXFLAGS="${CXXFLAGS}"
+export CROSSLDFLAGS="${LDFLAGS}"
 
-if [ "$BUILD_DEBUG" = "1" ]; then
-  CFLAGS+=" -g"; CROSSCFLAGS+=" -g"; CROSSCXXFLAGS+=" -g"
-fi
-
-# Prepare the build environment
-mkdir -p wine32-build wine64-build 
+# Prepare/configure, build and install Wine
 sudo mkdir -p wine-src/wine-install
+mkdir -p wine-build && cd wine-build || exit 1
 
-###############################################################################
-# Build 64-bit Wine
-###############################################################################
 echo
-echo "Preparing build environment for 64-bit Wine..."
+echo "Preparing build environment for Wine..."
 echo
 sleep 1.35
 
-cd wine64-build
-sudo apt install -y samba-dev libcups2-dev
-
 if [ "$BUILD_WAYLAND" = "0" ]; then
   ../wine-src/configure --prefix=/wine-builder/wine-src/wine-install \
-    --enable-opencl --enable-win64 --without-oss --without-wayland \
-    --disable-winemenubuilder --disable-win16 --disable-tests
+    --disable-tests --disable-win16 --disable-winemenubuilder \
+    --enable-opencl --without-oss --without-wayland \
+    --enable-archs=x86_64,i386 || exit 1
 else
   ../wine-src/configure --prefix=/wine-builder/wine-src/wine-install \
-    --enable-opencl --enable-win64 --without-oss \
-    --disable-winemenubuilder --disable-win16 --disable-tests
+    --disable-tests --disable-win16 --disable-winemenubuilder \
+    --enable-opencl --without-oss \
+    --enable-archs=x86_64,i386 || exit 1
 fi
 
-echo "Building 64-bit Wine..."
+echo "Building Wine..."
 echo
-( make -j$BUILD_THREADS >/dev/null
+( make -j$BUILD_THREADS >/dev/null || exit 1
 ) 2>&1 | grep -Ev "(aqs|parser|sql)\.y: (warning|note):"
 
-# Install 64-bit Wine
-sudo make install -j$BUILD_THREADS >/dev/null
-
-###############################################################################
-# Build 32-bit Wine
-###############################################################################
-echo
-echo "Preparing build environment for 32-bit Wine..."
-echo
-sleep 1.35
-
-cd ../wine32-build
-sudo apt install -y samba-dev:i386 libcups2-dev:i386
-
-if [ "$BUILD_WAYLAND" = "0" ]; then
-  PKG_CONFIG_PATH=/usr/lib/pkgconfig \
-    ../wine-src/configure --prefix=/wine-builder/wine-src/wine-install \
-    --with-wine64=../wine64-build --without-oss --without-wayland \
-    --disable-winemenubuilder --disable-win16 --disable-tests
-else
-  PKG_CONFIG_PATH=/usr/lib/pkgconfig \
-    ../wine-src/configure --prefix=/wine-builder/wine-src/wine-install \
-    --with-wine64=../wine64-build --without-oss \
-    --disable-winemenubuilder --disable-win16 --disable-tests
-fi
-
-echo "Building 32-bit Wine..."
-echo
-( make -j$BUILD_THREADS >/dev/null
-) 2>&1 | grep -Ev "(aqs|parser|sql)\.y: (warning|note):"
-
-# Install 32-bit Wine
-sudo make install -j$BUILD_THREADS >/dev/null
+sudo make install -j$BUILD_THREADS >/dev/null || exit 1
 
 echo
 echo "Build complete, final output is in 'wine-install'"
